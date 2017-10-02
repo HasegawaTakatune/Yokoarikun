259580a210580af36c728f1176543403d3ca4eb5
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-10-03 00:02:30 +0900

    画面サイズの設定

--- Yokoarikun2D/Assets/Script/StableAspect.cs
+++ Yokoarikun2D/Assets/Script/StableAspect.cs
@@ -8,10 +8,10 @@ public class StableAspect : MonoBehaviour {
 	//	呼び出し関係図
 	//	Awake
 	//******************************************************************//
-	private Camera cam;					// カメラコンポーネント
-	private float width  = 	720;		// カメラの横幅
-	private float height = 1280;		// カメラの縦幅
-	private float pixelPerUni=100f;		// ユニット当たりのピクセル(Pixel Per Unit)
+	//private Camera cam;					// カメラコンポーネント
+	//private float width  = 	720;		// カメラの横幅
+	//private float height = 1280;		// カメラの縦幅
+	//private float pixelPerUni=100f;		// ユニット当たりのピクセル(Pixel Per Unit)
 
 	//**************************************************************//
 	//	関数名　:	Awake
@@ -21,7 +21,7 @@ public class StableAspect : MonoBehaviour {
 	//	戻り値	:	なし
 	//**************************************************************//
 	void Awake(){
-		float aspect = (float)Screen.height / (float)Screen.width;	// 現在のアスペクト比
+		/*float aspect = (float)Screen.height / (float)Screen.width;	// 現在のアスペクト比
 		float bgAcpect = height / width;							// 設定したいアスペクト比
 
 		cam = GetComponent<Camera>();								// カメラコンポーネントを取得する
@@ -36,12 +36,12 @@ public class StableAspect : MonoBehaviour {
 			float bgScale=width/Screen.width;								// 倍率
 			float camHeight=height/(Screen.height*bgScale);					// viewport rectの幅
 			cam.rect = new Rect(0f, (1f - camHeight) / 2f, 1f, camHeight);	// viewport rectを設定
-		}
+		}*/
 
-		Screen.autorotateToLandscapeLeft = false;	// 画面を左に自動回転させるのを無効化
-		Screen.autorotateToLandscapeRight = false;	// 画面を右に自動回転させるのを無効化
-		Screen.autorotateToPortrait = true;			// 画面を上に自動回転させるのを有効化
-		Application.targetFrameRate = 60;			// フレームレートを60fpsに設定
+		//Screen.autorotateToLandscapeLeft = false;	// 画面を左に自動回転させるのを無効化
+		//Screen.autorotateToLandscapeRight = false;	// 画面を右に自動回転させるのを無効化
+		//Screen.autorotateToPortrait = true;			// 画面を上に自動回転させるのを有効化
+		//Application.targetFrameRate = 60;			// フレームレートを60fpsに設定
 	}
 
 
